65173c5a231aee487af2107f850125545825c8e9
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-02 15:53:09 +0600

    add deletebutton

--- src/components/Dashboard/AllProducts.js
+++ src/components/Dashboard/AllProducts.js
@@ -1,9 +1,91 @@
-import React from "react";
+import { Dropdown } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const AllProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [refress, setRefress] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/product")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setProducts(data.data);
+        } else {
+          toast.error(data.error);
+        }
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  }, [refress]);
+
+  console.log(products);
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/product/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          toast.success(data.message);
+          setRefress(!refress);
+        } else {
+          toast.error(data.error);
+        }
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
+
   return (
-    <div>
-      <h2>Product section</h2>
+    <div className="w-full">
+      <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="py-3 px-6">
+                Image
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Name
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Price
+              </th>
+              <th scope="col" class="py-3 px-6">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => {
+              return (
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <img className="w-20" src={product.url} alt="" />
+                  </th>
+                  <td class="py-4 px-6">{product.name}</td>
+                  <td class="py-4 px-6">${product.price}</td>
+                  <td class="py-4 px-6">
+                    <Dropdown label="Action" arrowIcon={false}>
+                      <Dropdown.Item>Edit</Dropdown.Item>
+                      <Dropdown.Item onClick={() => handleDelete(product._id)}>
+                        Delete
+                      </Dropdown.Item>
+                    </Dropdown>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
